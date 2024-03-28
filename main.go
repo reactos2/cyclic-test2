d48cf955895db6fa3eb691c82d445dcf81afc1e5
reactos2 <164987684+reactos2@users.noreply.github.com>
2024-03-28 07:17:44 +0000

    chi

--- main.go
+++ main.go
@@ -1,46 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	gin.SetMode(gin.ReleaseMode)
-	port := os.Getenv("PORT")
-	fmt.Println("port check 1: ", port)
-	if port == "" {
-		port = "8080"
-	}
-	fmt.Println("port check 2: ", port)
-
-	r := gin.Default()
-
-	r.GET("/", handleHomePage)
-	r.GET("/greeter/greet", handleGreeter)
-	r.GET("/test2", handleTest2)
-
-	r.GET("/:name", func(c *gin.Context) {
-		name := c.Param("name")
-
-		c.JSON(200, gin.H{
-			"message": fmt.Sprintf("Hello, %s!", name),
-		})
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
 	})
-
-	fmt.Println("port check 3: ", port)
-	r.Run(fmt.Sprintf(":%s", port))
-}
-
-func handleHomePage(ctx *gin.Context) {
-	ctx.String(200, "welcome to koyeb test 1")
-}
-func handleGreeter(ctx *gin.Context) {
-	ctx.String(200, "welcome to koyeb test 1")
-}
-func handleTest2(ctx *gin.Context) {
-	fmt.Println("http get test2")
-	ctx.String(200, "koyeb test 2")
+	http.ListenAndServe(":3000", r)
 }
