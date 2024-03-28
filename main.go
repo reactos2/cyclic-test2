5cf8796e495e2789fae392dd5c06e3952b3f3520
reactos2 <164987684+reactos2@users.noreply.github.com>
2024-03-28 07:02:27 +0000

    http

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello cyclic")
+	gin.SetMode(gin.ReleaseMode)
+	port := os.Getenv("PORT")
+	fmt.Println("port check 1: ", port)
+	if port == "" {
+		port = "8080"
+	}
+	fmt.Println("port check 2: ", port)
+
+	r := gin.Default()
+
+	r.GET("/", handleHomePage)
+	r.GET("/greeter/greet", handleGreeter)
+	r.GET("/test2", handleTest2)
+
+	r.GET("/:name", func(c *gin.Context) {
+		name := c.Param("name")
+
+		c.JSON(200, gin.H{
+			"message": fmt.Sprintf("Hello, %s!", name),
+		})
+	})
+
+	fmt.Println("port check 3: ", port)
+	r.Run(fmt.Sprintf(":%s", port))
+}
+
+func handleHomePage(ctx *gin.Context) {
+	ctx.String(200, "welcome to koyeb test 1")
+}
+func handleGreeter(ctx *gin.Context) {
+	ctx.String(200, "welcome to koyeb test 1")
+}
+func handleTest2(ctx *gin.Context) {
+	fmt.Println("http get test2")
+	ctx.String(200, "koyeb test 2")
 }
